f432e2007c85834581d50720a518fdf796257956
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2023-05-17 18:42:27 +0100

    changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import GlobalStyles from "@/styles/GlobalStyles";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
-      <Toaster />
+
       <Component {...pageProps} />
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,6 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import tw from "twin.macro";
 
-const Box = tw.div`border-[1px] border-black h-[240px] w-[256px] rounded-lg`;
-const OverFlow = tw(Row)`overflow-x-auto flex-nowrap gap-x-10 `;
-
 export default function Home() {
   const router = useRouter();
   React.useEffect(() => {
