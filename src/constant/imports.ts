838ef488df615c07a402e1023c2001cadb1caa90
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2023-05-17 18:37:52 +0100

    changes

--- src/constant/imports.ts
+++ src/constant/imports.ts
@@ -6,14 +6,6 @@ import React, {
   Component,
   useCallback,
 } from "react";
-import { useFormik } from "formik";
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import { useDispatch, useSelector } from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { setupListeners } from "@reduxjs/toolkit/dist/query";
-import { persistReducer, persistStore } from "redux-persist";
 
 export {
   // React imports
@@ -25,17 +17,4 @@ export {
   Component,
   useCallback,
   // Store imports
-  configureStore,
-  Provider,
-  useDispatch,
-  useSelector,
-  TypedUseSelectorHook,
-  createApi,
-  fetchBaseQuery,
-  setupListeners,
-  createSlice,
-  persistReducer,
-  persistStore,
-  // form imports
-  useFormik,
 };
